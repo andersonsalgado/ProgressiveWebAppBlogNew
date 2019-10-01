6d577fc1878a3bacf3377199712fbfbbdbd1298b
Logon Aluno <logonaluno@local.com>
2019-09-30 21:23:05 -0300

    criacao js

--- wwwroot/js/blog.js
+++ wwwroot/js/blog.js
@@ -1,1 +1,33 @@
-﻿
+﻿function fetchPromise(url) {
+    return new Promise(function (resolve, reject) {
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                clientStorage.addPosts(data)
+                    .then(function () {
+                        resolve('The connection is OK, showing latest results');
+                    });
+            }).catch(function (e) {
+                resolve('No connection, showing offline results');
+            });
+        setTimeout(function () {
+            resolve('The connection is hanging, showing offline results');
+        }, 1000);
+    });
+}
+
+function loadData(url) {
+    fetchPromise(url)
+        .then(function (status) {
+            $('#connection-status').html(status);
+            clientStorage.getPosts()
+                .then(function (posts) {
+                    template.appendBlogList(posts);
+                })
+        });
+}
+function loadMoreBlogPosts() {
+    loadData(blogMorePostsUrl +
+        clientStorage.getOldestBlogPostId());
+}
