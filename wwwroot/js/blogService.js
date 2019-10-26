f9c3407f9aef9031cefd720d1b5112d0fa648ca8
Anderson Salgado <anderson.desen@gmail.com>
2019-10-26 18:48:29 -0300

    Material 2/ Metade.

--- wwwroot/js/blogService.js
+++ wwwroot/js/blogService.js
@@ -1,6 +1,9 @@
-﻿define(['js/template.js'], function (template) {
+﻿define(['js/template.js,js/clientStorage.js'], function (template, clientStorage) {
 
     var oldestBlogPostId = 0;
+
+    
+
     function setOldestBlogPostId(data) {
         var ids = data.map(item => item.postId);
         oldestBlogPostId = Math.min(...ids);
@@ -38,17 +41,54 @@
     
     
     function loadMoreBlogPosts() {
-        fetch(blogMorePostsUrl + oldestBlogPostId)
+        //fetch(blogMorePostsUrl + oldestBlogPostId)
+        //    .then(function (response) {
+        //        return response.json();
+        //    }).then(function (data) {
+        //        console.log(data);
+        //        template.appendBlogList(data);
+        //        setOldestBlogPostId(data);
+        //    });
+
+        loadData(blogMorePostsUrl + clientStorage.getOldestBlogPostId());
+
+    }
+
+    function fetchPromise(url) {
+        return new Promise(function (resolve, reject) {
+            fetch(url)
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
-                console.log(data);
-                template.appendBlogList(data);
-                setOldestBlogPostId(data);
-            });
-    }
+                clientStorage.addPosts(data);
+            }).then(function () {
+                resolve('The connection is OK, showing latest results');
+            }).catch(function (e) {
+                resolve('No connection, showing offline results');
+            }); 
 
+            setTimeout(function () {
+                resolve('The connection is hanging, showing offline results');
+            }, 1000);
 
+        });
+    } 
+
+    function loadData(url) {
+        fetchPromise(url)
+        .then(function (status) {
+            $('#connection-status').html(status);
+            clientStorage.getPosts();
+        })
+        .then(function (post) {
+            template.appendBlogList(posts);
+        });
+    } 
+
+    
+    
+
+    
 
     return {
         loadLatestBlogPosts: loadLatestBlogPosts,
--- wwwroot/js/clientStorage.js
+++ wwwroot/js/clientStorage.js
@@ -1,75 +1,51 @@
-﻿
-define([], function () {
-    var blogInstance = localforage.createInstance({
-        name: 'blog'
-    });
+﻿define([], function () {
+
+    var blogInstance = localforage.createInstance({ name: 'blog' });
+
+    function getOldestBlogPostId() {
+        return oldestBlogPostId;
+    }
+
     function addPosts(posts) {
         return new Promise(function (resolve, reject) {
+
             var keyValuePair = [];
             posts.map(function (item) {
-                keyValuePair.push({
-                    key: item.postId, value: item
-                });
-            })
-            blogInstance.setItems(keyValuePair)
-                .then(function () {
-                    resolve();
-                });
-        });
-    }
-    return {
-        addPosts: addPosts
-    }
-});
-var oldestBlogPostId = null;
-var limit = 3;
-function getPosts() {
-    return new Promise(function (resolve, reject) {
-        blogInstance.keys().then(function (keys) {
-            var index = keys.indexOf(oldestBlogPostId);
-            if (index == -1) { index = keys.length; }
-            if (index == 0) { resolve([]); return; }
-            var start = index - limit;
-            var limitAdjusted = start < 0 ? index : limit;
-            var keys = keys.splice(Math.max(0, start),
-                limitAdjusted);
-            blogInstance.getItems(keys).then(function
-                (results) {
-                var posts =
-                    Object.keys(results).map(function (k) {
-                        return results[k]
-                    }).reverse();
-                oldestBlogPostId = posts[posts.length -
-                    1].postId;
-                resolve(posts);
+                keyValuePair.push({ key: item.postId, value: item });
             });
-        });
-    });
-}
-function getOldestBlogPostId() {
-    return oldestBlogPostId;
-}
 
-function getPostText(link) {
-    return new Promise(function (resolve, reject) {
-        blogInstance.getItem('#' + link)
-            .then(function (text) {
-                resolve(text);
-            });
-    });
-}
-function addPostText(link, text) {
-    return new Promise(function (resolve, reject) {
-        blogInstance.setItem('#' + link,
-            text).then(function () {
+            blogInstance.setItems(keyValuePair).then(function () {
                 resolve();
+            }); 
+        });
+    }
+
+
+    var limit = 2;
+    function getPosts() {
+        return new Promise(function (resolve, reject) {
+            blogInstance.keys().then(function (_keys) {
+                var index = _keys.indexOf(oldestBlogPostId);
+                if (index === -1) { index = _keys.length; }
+                if (index === 0) { resolve([]); return; }
+                var start = index - limit;
+                var limitAdjusted = start < 0 ? index : limit;
+                var keys = _keys.splice(Math.max(0, start), limitAdjusted);
+                blogInstance.getItems(keys).then(function (results) {
+                    var posts = Object.keys(results).map(function (k) {
+                        return results[k];
+                    }).reverse();
+                    oldestBlogPostId = posts[posts.length - 1].postId;
+                    resolve(posts);
+                });
             });
-    });
-}
-return {
-    addPosts: addPosts,
-    getPosts: getPosts,
-    getOldestBlogPostId: getOldestBlogPostId,
-    addPostText: addPostText,
-    getPostText: getPostText
-}
+        });
+    }
+
+    return {
+        addPosts: addPosts,
+        getPosts: getPosts,
+        getOldestBlogPostId: getOldestBlogPostId
+    };
+
+});
